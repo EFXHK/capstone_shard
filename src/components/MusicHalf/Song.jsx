6a2b0f763559252e0ab45c8d38df70c677663b6c
esau fong <esaufong@gmail.com>
2023-01-10 01:06:07 +0000

    returns and extra bracket removed

--- src/components/MusicHalf/Song.jsx
+++ src/components/MusicHalf/Song.jsx
@@ -1,4 +1,4 @@
-
+import React, {useEffect, useState, useRef } from 'react';
 
 function Song({title, artist, position, image, audio}) {
 
@@ -10,12 +10,40 @@ function Song({title, artist, position, image, audio}) {
     const handlePlayPause = () => {
         setIsPlaying(!isPlaying)
     }
-}
-
-useEffect(() => {
-    //skip play on mount?
-    if (isPlaying === null) return
-    isPlaying ? audioSrc.current.play() : audioSrc.current.pause()
-}, [isPlaying])
 
 
+    useEffect(() => {
+        //skip play on mount?
+        if (isPlaying === null) return
+        isPlaying ? audioSrc.current.play() : audioSrc.current.pause()
+    }, [isPlaying])
+
+    return ( 
+        <div className="song">
+            <img
+            alt={"Play " + altTag}
+            id={position}
+            className={`audio-control ${isPlaying ? "playing" : ""}`}
+            src='https://image.freepik.com/free-icon/play-button_318-42541.jpg'
+            onClick={handlePlayPause}
+            />
+
+            <img
+            src={image}
+            alt={altTag} />
+
+            <div className= 'details'>
+                <h3>{position}. {title}</h3>
+                <h4>{artist}</h4>
+            </div>
+
+            <audio
+                ref={audioSrc}
+                id={'audio' + position}
+                src={audio}
+                />
+        </div>
+    );
+};
+
+export default Song;
