ba7c674dac737a2ff9fe1feb964ea459462e1fc5
esau fong <esaufong@gmail.com>
2023-01-10 01:26:54 +0000

    ChartContainer added

---
+++ src/components/MusicHalf/ChartContainer.jsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import Chart from "./Chart";
+import Titlebar from "./TitleBar";
+
+
+
+// swap hard coded genres
+const ChartContainer = ({ genres }) => {
+    const [songs, setSongs] = useState([]);
+
+    useEffect(() => {
+        loadSongs(genres[0].url)
+    }, [genres])
+
+    const loadSongs = url => {
+        fetch(url)
+            .then(res => res.json())
+            .then(songList => setSongs(songList.feed.entry))
+            .catch(err => console.error);
+    }
+
+    const handleSelectChange = event => {
+        loadSongs(event.target.value);
+    }
+
+    return (
+        <>
+            <Titlebar
+                handleSelectChange={handleSelectChange}
+                genres={genres}
+            />
+            <Chart
+                songs={songs}
+                url={genres[0].url}
+                handleSelectChange={handleSelectChange}
+                />
+        </>
+    )
+}
+
+export default ChartContainer;
+
